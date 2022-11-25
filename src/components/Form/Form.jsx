5312c97030b34f97b1dca2eaa6542ba76558e148
Alex Jiménez <alexjimenezada@gmail.com>
2022-11-25 14:00:29 +0100

    Random font for article title and form add new article

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Form.scss'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <main className='form-view'>
+      <div className='form-container'>
+        <form>
+          <h1>New article</h1>
+
+          <label htmlFor='title'>Title:</label>
+          <input type="text" id='title' name='title' className='title' />
+
+          <label htmlFor='author'>Author:</label>
+          <input type="text" id='author' name='author' className='author' />
+
+          <label htmlFor='image'>Url image:</label>
+          <input type="text" id='image' name='image' className='image' />
+
+          <label htmlFor='abstract'>Abstract:</label>
+          <textarea id='abstract' name='abstract' className='abstract' rows='4'></textarea>
+
+          <button type='submit' className='submit'>Submit</button>
+
+        </form>
+      </div>
+    </main>
   )
 }
 
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,11 +4,12 @@ import './News.scss'
 
 const News = () => {
     const { articles } = useContext(GlobalContext);
+    const fonts = ['playfair', 'red']
 
-    const news = articles.filter(article => article.title !== '').map((article) => {
+    const news = articles.filter(article => article.title !== '').map((article, idx) => {
         return (
-          <div className='new-card'>
-            <div className='new-card-title'>{ article.title }</div>
+          <div className='new-card' key={idx}>
+            <div className={`new-card-title ${fonts[Math.floor(Math.random()*fonts.length)]}`}>{ article.title }</div>
             <div className='new-card-byline'>{ article.byline }</div>
             <div className='new-card-body'>
                 { article.multimedia ? <img className='new-card-image' src={article.multimedia[0].url} alt="Article multimedia" /> : null }
