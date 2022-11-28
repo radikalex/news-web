ebad8938011ddf67441f1bfaba8392e75868f139
Alex Jimenez <alexjimenezada@gmail.com>
2022-11-29 00:37:16 +0100

    Footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="contenedor-copyright">
-        <span className="copyright">Todos los derechos reservados por Alex Jimenez.</span><span className="copyright"> Copyright © 2022</span>
+        <span className="copyright">All rights reserved by Alex Jimenez.</span><span className="copyright"> Copyright © 2022</span>
       </div>
     </footer>
   )
