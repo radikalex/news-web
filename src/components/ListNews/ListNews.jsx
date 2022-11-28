14e5b83a3b247929a5f10cbaa1575215df7851f1
Alex Jiménez <alexjimenezada@gmail.com>
2022-11-28 14:30:58 +0100

    Filter articles by title

--- src/components/ListNews/ListNews.jsx
+++ src/components/ListNews/ListNews.jsx
@@ -7,6 +7,7 @@ import './ListNews.scss'
 const ListNews = () => {
   const { getArticles, loading } = useContext(GlobalContext);
   const [section, setSection] = useState('arts');
+  const [titleFilter, setTitleFilter] = useState('');
   
   useEffect(() => {
     getArticles(section);
@@ -15,47 +16,55 @@ const ListNews = () => {
 
   const handleSelectChange = (e) => {
     setSection(e.target.value);
+    setTitleFilter('');
+  }
+
+  const handleFilterChange = (e) => {
+    setTitleFilter(e.target.value);
   }
 
   return (
     <main>
       <div className="newspaper-container">
         <form className="form-section">
+          <input type='text' className="filter" id='filter' placeholder='Filter by title name...' value={titleFilter} onChange={handleFilterChange}/>
+          <div className="section-container">
           <label htmlFor="section" className="label-section">News section: </label>
-          <select name="section" id="section" className="section" onChange={handleSelectChange} value={section}>
-            <option value="arts">Arts</option>
-            <option value="books">Books</option>
-            <option value="business">Business</option>
-            <option value="fashion">Fashion</option>
-            <option value="food">Food</option>
-            <option value="health">Health</option>
-            <option value="home">Home</option>
-            <option value="insider">Insider</option>
-            <option value="magazine">Magazine</option>
-            <option value="movies">Movies</option>
-            <option value="nyregion">NY Region</option>
-            <option value="obituaries">Obituaries</option>
-            <option value="opinion">Opinion</option>
-            <option value="politics">Politics</option>
-            <option value="realestate">Real Estate</option>
-            <option value="science">Science</option>
-            <option value="sports">Sports</option>
-            <option value="sundayreview">Sunday Review</option>
-            <option value="technology">Technology</option>
-            <option value="theater">Theater</option>
-            <option value="t-magazine">T-Magazine</option>
-            <option value="travel">Travel</option>
-            <option value="upshot">Upshot</option>
-            <option value="us">US</option>
-            <option value="world">World</option>
-          </select>
+            <select name="section" id="section" className="section" onChange={handleSelectChange} value={section}>
+              <option value="arts">Arts</option>
+              <option value="books">Books</option>
+              <option value="business">Business</option>
+              <option value="fashion">Fashion</option>
+              <option value="food">Food</option>
+              <option value="health">Health</option>
+              <option value="home">Home</option>
+              <option value="insider">Insider</option>
+              <option value="magazine">Magazine</option>
+              <option value="movies">Movies</option>
+              <option value="nyregion">NY Region</option>
+              <option value="obituaries">Obituaries</option>
+              <option value="opinion">Opinion</option>
+              <option value="politics">Politics</option>
+              <option value="realestate">Real Estate</option>
+              <option value="science">Science</option>
+              <option value="sports">Sports</option>
+              <option value="sundayreview">Sunday Review</option>
+              <option value="technology">Technology</option>
+              <option value="theater">Theater</option>
+              <option value="t-magazine">T-Magazine</option>
+              <option value="travel">Travel</option>
+              <option value="upshot">Upshot</option>
+              <option value="us">US</option>
+              <option value="world">World</option>
+            </select>
+          </div>
         </form>
         { loading 
           ? <Loading /> 
           : <div className="newspaper">
               <div className="newspaper-header">Radikalex Post</div>
               <div className="newspaper-subhead">{Date().slice(0, 16)}</div>
-              <News />
+              <News filter={titleFilter}/>
             </div>
         }
       </div>
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,22 +2,27 @@ import React, { useContext } from 'react'
 import { GlobalContext } from '../../context/GlobalState';
 import './News.scss'
 
-const News = () => {
+const News = (props) => {
     const { articles } = useContext(GlobalContext);
-    const fonts = ['playfair', 'noticia']
+    const fonts = ['playfair', 'noticia', 'chomsky', 'eroded']
 
-    const news = articles.filter(article => article.title !== '').map((article, idx) => {
+    const news = articles.map((article, idx) => {
         return (
-          <div className='new-card' key={idx}>
-            <div className={`new-card-title ${fonts[Math.floor(Math.random()*fonts.length)]}`}>{ article.title }</div>
-            <div className='new-card-byline'>{ article.byline }</div>
-            <div className='new-card-body'>
-                { article.multimedia ? <img className='new-card-image' src={article.multimedia[0].url} alt="Article multimedia" /> : null }
-                <p>{ article.abstract }</p>
-                <p>{ article.abstract }</p>
-                <p>{ article.abstract }</p>
-            </div>
-          </div>
+            <React.Fragment key={idx}>
+            {   article.title !== '' && (new RegExp(`${props.filter}`, 'gmi')).test(article.title)
+                ? <div className='new-card'>
+                    <div className={`new-card-title ${fonts[( (/[0-9]/gm).test(article.title) ? idx % (fonts.length - 1) : idx % fonts.length)]}`}>{ article.title }</div>
+                    <div className='new-card-byline'>{ article.byline }</div>
+                    <div className='new-card-body'>
+                        { article.multimedia ? <img className='new-card-image' src={article.multimedia[0].url} alt="Article multimedia" /> : null }
+                        <p>{ article.abstract }</p>
+                        <p>{ article.abstract }</p>
+                        <p>{ article.abstract }</p>
+                    </div>
+                  </div>
+                : null
+            }
+          </ React.Fragment>
         );
     });
 
